facfd12fbd637031b07380d6abcbe3bacdea7749
Julian C <iuliancrisan01@gmail.com>
2021-08-18 15:04:30 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const App = () => {
+  return (
+    <Container>
+      <h1>Movie characters</h1>
+    </Container>
+  )
+}
+
+export default App
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
